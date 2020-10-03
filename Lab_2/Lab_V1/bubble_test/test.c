8997295501148a83e863dec3bda13e47cb86e65a
Krasavin Georgy <gosha200242@gmail.com>
2020-10-04 00:21:05 +0300

    done

---
+++ Lab_2/Lab_V1/bubble_test/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define N 5
+
+int main()
+{
+    int mm[N];
+    int t;
+    for(int i = 0; i < N; i++)
+    {
+        scanf_s("%d", &mm[i]);
+    }
+    for(int i = 0; i < N; i++)
+    {
+        printf("%d ", mm[i]);
+    }
+    for(int i = 0; i < N - 1; i++)
+    {
+        for(int j = 0 ; j < N - i - 1 ; j++)
+        {
+            if(mm[j] > mm[j+1]) 
+            {
+                t = mm[j];               
+                mm[j] = mm[j + 1];
+                mm[j + 1] = t; 
+            }
+        }  
+    }
+    printf("\n");
+    for(int i = 0; i < N; i++)
+    {
+        printf("%d ", mm[i]);
+    }
+    return 0;
+}
