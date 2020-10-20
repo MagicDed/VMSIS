33df015a3d1fa70073dd9589dc449224a1f635c3
Krasavin Georgy <gosha200242@gmail.com>
2020-10-20 15:46:18 +0300

    done

--- Collection/Lab_4/#3/3.c
+++ Collection/Lab_4/#3/3.c
@@ -1,64 +1,66 @@
+// В матрице найти седловую точку
+
 #include <stdio.h>
 #include <stdlib.h>
-// #include <iostream>
+// #include <iostream>                  // Для VS
 
 int main()
 {
-    int x, t, N, max;
-    int **mas;
+    int x, t, N, max;                   // Повтор программы(x) Временная(t) Размер матрицы(N) Макс число(max) 
+    int dot = 0;                        // Седловая точка
+    int **mas;                          // Указатель на матрицу
     do
     {
-        // system("CLS");
-        int dot = 0;
-        printf("What's the size of the matrix cube? ");
-        scanf_s("%d", &N);
-        mas = (int**)malloc(N * sizeof(int*));
-        for(int i = 0; i < N; i++)
+        // system("CLS");                                                           // Для VS
+        printf("What's the size of the matrix cube? ");                             // Вывод текста (какой размер матрицы)
+        scanf_s("%d", &N);                                                          // Присваивание числа
+        mas = (int**)malloc(N * sizeof(int*));                                      // Выделение памяти под строчки
+        for(int i = 0; i < N; i++)                                                  // Цикл для выделения памяти под столбцы
         {
-            mas[i] = (int*)malloc(N * sizeof(int));
+            mas[i] = (int*)malloc(N * sizeof(int));                                     // Выделение памяти под столбцы
         }
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i < N; i++)                                                  // Цикл для ввода цифр в матрицу
         {
             for(int j = 0; j < N; j++)
             {
-                printf("Write a number: ");
-                scanf_s("%d", &mas[i][j]);
+                printf("Write a number: ");                                                 // Вывод надписи(введите число)
+                scanf_s("%d", &mas[i][j]);                                                  // Присваивание числа к матрице
             }
         }
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i < N; i++)                                                  // Цикл вывода массива
         {
             for(int j = 0; j < N; j++)
             {
-                printf("%d\t", mas[i][j]);
+                printf("%d\t", mas[i][j]);                                                  // Выводит массив
             }
-            printf("\n");
+            printf("\n");                                                               // Переход на новую линию
         }
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i < N; i++)                                                  // Cканирование линии
         {
-            max = mas[i][0];
-            for(int j = 0; j < N; j++)
+            max = mas[i][0];                                                            // Первое число в линии и есть максимальное
+            for(int j = 0; j < N; j++)                                                  // Ишем максимальное число в линии
             {
-                if(mas[i][j] > max)
+                if(mas[i][j] > max)                                                         // Если число больше макс то
                 {
-                    max = mas[i][j];
-                    t = j;
+                    max = mas[i][j];                                                            // То оно новое макс
+                    t = j;                                                                      // Запоминаем его место
                 }
             }
-            for(int k = 0; k < N; k++)
+            for(int k = 0; k < N; k++)                                                  // Цикл чтобы узнать является ли макс минимальным числом в столбце
             {
-                if(max < mas[k][t] && (!i == k))
+                if(max < mas[k][t] && (!i == k))                                            // Если меньше и не равен самому себе то
                 {
-                    dot = max;
+                    dot = max;                                                                  // Дот = макс
                 }
-                if(max > mas[k][t] && (!i == k))
+                if(max > mas[k][t] && (!i == k))                                            // Если больше и не равен самому себе то
                 {
-                    break;
+                    break;                                                                      // То выход из цикла
                 } 
             }
         }
-        printf("Sedlovaya dot: %d\n", dot);
-        printf("Do you want to repeat the program?\n1. Yes\n2. No\nAnswer: ");
-        scanf_s("%d", &x);
-    } while (x == 1);
+        printf("Sedlovaya dot: %d\n", dot);                                         // Вывод максимального числа
+        printf("Do you want to repeat the program?\n1. Yes\n2. No\nAnswer: ");      // Вывод повтора программы да/нет
+        scanf_s("%d", &x);                                                          // Если 1
+    } while (x == 1);                                                           // То повторяем
     return 0;
 }
