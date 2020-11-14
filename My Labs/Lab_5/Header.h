72a8b0a8192cccdee1a7c390674c25b9a37308f9
Krasavin Georgy <gosha200242@gmail.com>
2020-11-14 11:55:23 +0300

    done

--- My Labs/Lab_5/Header.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-// #include <iostream>                          // Для VS
-
-int** AllocateMemory(int**, int, int);          // Выделение памяти
-void Input(int**, int, int);                    // Ввод чисел в матрицу
-void Output(int**, int, int);                   // Вывод матрицы
-void Calculations(int**, int, int);             // Вычисления в матрице
