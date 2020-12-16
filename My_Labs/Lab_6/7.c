923bec6c3c90ad54bf9ba6f875d31a8843226d13
Krasavin Georgy <gosha200242@gmail.com>
2020-12-16 21:50:10 +0300

    done

--- My_Labs/Lab_6/7.c
+++ My_Labs/Lab_6/7.c
@@ -1,5 +1,6 @@
 // Форматировать строчку таким образом чтобы 3 число и последйщие числа удалялись если перед ними стояла точка
 
+// The task is to remove all the numbers after the second one if there was a dot before it so .0123 become .01
 #include "Header.h"
 #include "Functionlist6.7.c"
 
@@ -17,6 +18,7 @@ int main()
         Input(RawLine, Length);                                 // Ввод сиволов в строку
         Line = Calculations(RawLine, Line, Length);             // Вычисления
         Output(Line);                                           // Вывод линии
+        //free(RawLine);  free(Line);                             // AINT DAMN WORKING AAA
 		printf("Do you want to repeat program? y/n\n");			// Повтор программы y/n
 		rewind(stdin);											// Чистка буфера
 		scanf_s("%c", &x);
