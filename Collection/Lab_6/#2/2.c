77552b42f4b243801cc5711cda9adf9a09881f76
Krasavin Georgy <gosha200242@gmail.com>
2020-12-17 23:36:09 +0300

    done

--- Collection/Lab_6/#2/2.c
+++ Collection/Lab_6/#2/2.c
@@ -10,16 +10,16 @@ int main()
     char *Line;             // Форматированая строчка
     int Length = NULL;      // Длинна строк
     do
-	{
-		Length = StringLength(Length);                          // Ввод длинны строки
+    {
+        Length = StringLength(Length);                          // Ввод длинны строки
         RawLine = MemoryAllocation(Length);                     // Выделение памяти под первую строку
         Line = MemoryAllocation(Length);                        // Выделение памяти под вторую строку
         Input(RawLine, Length);                                 // Ввод сиволов в строку
         Line = Calculations(RawLine, Line, Length);             // Вычисления
         Output(Line);                                           // Вывод линии
-		printf("Do you want to repeat program? y/n\n");			// Повтор программы y/n
-		rewind(stdin);											// Чистка буфера
-		scanf_s("%c", &x);
-	} while (x == 'y');
+        printf("Do you want to repeat program? y/n\n");			// Повтор программы y/n
+        rewind(stdin);											// Чистка буфера
+        scanf_s("%c", &x);
+    } while (x == 'y');
     return 0;
 }
